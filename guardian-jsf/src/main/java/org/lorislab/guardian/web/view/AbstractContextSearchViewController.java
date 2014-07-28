4a09c1bfcc35b62d36de0d243824bd32644f3e25
Andrej Petras <andrej@ajka-andrej.com>
2014-07-28 22:34:31 +0200

    Update

---
+++ guardian-jsf/src/main/java/org/lorislab/guardian/web/view/AbstractContextSearchViewController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 Andrej Petras.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lorislab.guardian.web.view;
+
+import javax.inject.Inject;
+import org.lorislab.guardian.api.model.UserData;
+import org.lorislab.jel.base.criteria.AbstractSearchCriteria;
+import org.lorislab.jel.jsf.view.AbstractSearchViewController;
+
+/**
+ *
+ * @author Andrej Petras
+ */
+public abstract class AbstractContextSearchViewController<T, S extends  AbstractSearchCriteria> extends AbstractSearchViewController<T, S> implements ActionContextViewController {
+    
+    private static final long serialVersionUID = -3077597243187865726L;
+
+    @Inject
+    protected UserData userData;
+    
+    @Override
+    public boolean hasUserAction(Enum context, Enum action) {
+        if (userData != null) {
+            return userData.hasAction(context, action);
+        }
+        return false;
+    }
+    
+}
